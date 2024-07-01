b756518839086d9083f59aa6ff187a480bd086b7
MrChukwudi <chukwudidesire01@gmail.com>
2024-07-01 00:47:29 -0400

    Ignored .vs directory

---
+++ CalculatorEngine.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalc
+{
+	internal class CalculatorEngine
+	{
+		//Method to Perform Addition
+		public double Add(double firstNumber, double secondNumber ) 
+		{
+			try
+			{
+				double result;
+				if(!double.IsNaN(firstNumber) && !double.IsNaN(secondNumber))
+				{
+					throw new ArgumentException("To perform the addition operation, you need a valid Number!");
+				}
+				result = firstNumber + secondNumber;
+
+				return result;
+			}catch(Exception ex)
+			{
+                Console.WriteLine("The problem is: {0}", ex.Message);
+				return 0;
+			}
+		}
+
+		//Method to perform Subtraction:
+		public double Subtract(double firstNumber, double secondNumber)
+		{
+			try
+			{
+				double result;
+				if (!double.IsNaN(firstNumber) && !double.IsNaN(secondNumber))
+				{
+					throw new ArgumentException("To perform the addition operation, you need a valid Number!");
+				}
+				result = firstNumber - secondNumber;
+
+				return result;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("The problem is: {0}", ex.Message);
+				return 0;
+			}
+		}
+
+
+		//Method to perform Division:
+		public double Divide(double firstNumber, double secondNumber)
+		{
+			try
+			{
+				double result;
+				if (!double.IsNaN(firstNumber) && !double.IsNaN(secondNumber))
+				{
+					throw new ArgumentException("To perform the addition operation, you need a valid Number!");
+				}
+				result = firstNumber / secondNumber;
+
+				return result;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("The problem is: {0}", ex.Message);
+				return 0;
+			}
+		}
+
+
+		//Method to perform Multiplication:
+		public double Multiply(double firstNumber, double secondNumber)
+		{
+			try
+			{
+				double result;
+				if (!double.IsNaN(firstNumber) && !double.IsNaN(secondNumber))
+				{
+					throw new ArgumentException("To perform the addition operation, you need a valid Number!");
+				}
+				result = firstNumber * secondNumber;
+
+				return result;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("The problem is: {0}", ex.Message);
+				return 0;
+			}
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalc
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Instanciate Calculator Engine:
+			CalculatorEngine engine = new CalculatorEngine();
+
+			//Instanciate Number Copnverter:
+			InputConverter converter = new InputConverter();
+
+            //Collect Inputs from User:
+            Console.WriteLine("Input your first Number");
+			double firstNum;
+			firstNum = converter.ConvertToNumber(Console.ReadLine());
+
+			Console.WriteLine("Input your Second Number");
+			double secondNum;
+			secondNum = converter.ConvertToNumber(Console.ReadLine());
+
+
+			Console.WriteLine("Input your Operator");
+			string myOperator = Console.ReadLine();
+
+			//Deciding the Operation by the Operator type:
+			switch(myOperator)
+			{
+				case "+":
+					Console.WriteLine(engine.Add(firstNum, secondNum));
+					break;
+
+				case "-":
+					Console.WriteLine(engine.Subtract(firstNum, secondNum));
+					break;
+
+				case "*":
+					Console.WriteLine(engine.Multiply(firstNum, secondNum));
+					break;
+
+				case "/":
+					Console.WriteLine(engine.Divide(firstNum, secondNum));
+					break;
+
+				default:
+                    Console.WriteLine("You didn not enter a Valid operator.");
+                    break;
+
+			}
+        }
+	}
+}
